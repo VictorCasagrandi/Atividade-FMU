f8170bc017dc32faa6788271432ae4ed0ae4bac0
Victor Casagrandi <victor_casagrandi@hotmail.com>
2019-10-05 12:42:58 -0300

    Fibonacci com Recursividade sem For.

---
+++ src/main/java/com/victor/api/FibonacciRecApplication.java
@@ -0,0 +1,49 @@
+package com.victor.api;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibonacciRecApplication {
+
+	private static Scanner scanner;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FibonacciRecApplication.class, args);
+		
+			scanner = new Scanner(System.in);
+
+			ArrayList<Integer> pSequencia = new ArrayList<Integer>();
+			
+			System.out.println("Informe a Sequência Fibonacci - ");
+			int vEntrada = scanner.nextInt();
+			
+			if (vEntrada == 1) {
+				pSequencia.add(0);
+				pSequencia.add(1);
+			} if (vEntrada >= 2) {
+				  pSequencia.add(0);
+				  pSequencia.add(1);
+				  pSequencia.add(1);
+			  }
+			
+			fibonacci(vEntrada, pSequencia);
+			System.out.println("Valor da Sequência: " + pSequencia);
+		}
+
+		static int fibonacci(int vNumero, ArrayList<Integer> pSequencia) {
+			
+			if (vNumero == 2 || vNumero == 1) {
+				return 1;
+			} else {
+				int vFibonacci = fibonacci(vNumero - 1, pSequencia) + fibonacci(vNumero - 2, pSequencia);
+				if (!pSequencia.contains(vFibonacci))
+					pSequencia.add(vFibonacci);
+				return vFibonacci;
+			}
+	}
+
+}
